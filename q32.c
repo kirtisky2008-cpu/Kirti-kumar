5715b425e3f67223b78cfe772b82511e255100c3
kirtisky2008-cpu <kirtisky2008@gmail.com>
2026-08-25 18:10:03 +0530

    Add files via upload

---
+++ q32.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, reverse = 0, remainder;
+
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
