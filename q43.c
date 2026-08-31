4faec13e64b7baf4613993a6b2dfdf4fe85aaf04
kirtisky2008-cpu <kirtisky2008@gmail.com>
2026-08-31 22:35:20 +0530

    Add files via upload

---
+++ q43.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, temp, digit, sum = 0, fact, i;
+
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        fact = 1;
+
+        for (i = 1; i <= digit; i++) {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+
+    if (sum == n)
+        printf("Strong number");
+    else
+        printf("Not strong number");
+
+    return 0;
+}
