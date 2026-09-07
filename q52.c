58f92fb7132ba153454e5ac48bcc611eaa5d8121
kirtisky2008-cpu <kirtisky2008@gmail.com>
2026-09-07 22:01:24 +0530

    Add files via upload

---
+++ q52.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("*\n\n");
+
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");
+
+    return 0;
+}
