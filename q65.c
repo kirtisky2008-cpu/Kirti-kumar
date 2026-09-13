e2c59896d1f9d29336aca8e36154635242094118
kirtisky2008-cpu <kirtisky2008@gmail.com>
2026-09-13 18:20:36 +0530

    Add files via upload

---
+++ q65.c
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cin >> target;
+
+    int low = 0, high = n - 1;
+    int result = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            result = mid;
+            break;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (result != -1)
+        cout << "Found at index " << result << endl;
+    else
+        cout << -1 << endl;
+
+    return 0;
+}
