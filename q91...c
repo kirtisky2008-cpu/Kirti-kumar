1b87036466ff9d430d9b5cbe896e18e883278daa
kirtisky2008-cpu <kirtisky2008@gmail.com>
2026-09-24 12:22:12 +0530

    Add files via upload

---
+++ q91...c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    int i;
+
+    fgets(s, sizeof(s), stdin);
+
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i]!='a' && s[i]!='e' && s[i]!='i' &&
+            s[i]!='o' && s[i]!='u')
+            printf("%c", s[i]);
+    }
+
+    return 0;
+}
